f3969e8f028ea9dfbdcc8db8b396a6a343ad55ab
SadDawgBro <123268265+SadDawgBro@users.noreply.github.com>
2023-12-26 00:47:39 +0000

    Update notification.js

--- stuff/notification.js
+++ stuff/notification.js
@@ -1,9 +1,4 @@
 function sendNotification(title, body) {
-  if (!('Notification' in window)) {
-    // console.log('This browser does not support notifications.');
-    alert('This browser does not support notifications.');
-  else {
-    Notification.requestPermission();
-    var notification = new Notification(title, { body: body});
-  }
+  Notification.requestPermission();
+  var notification = new Notification(title, { body: body});
 }
